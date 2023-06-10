5f5416e79819007fa19778cd502347cf5c5d99e2
sharanyakomma <96442414+sharanyakomma@users.noreply.github.com>
2023-06-10 21:01:57 +0530

    Update 01_simple_randomization.py

--- examples/01_simple_randomization.py
+++ examples/01_simple_randomization.py
@@ -7,6 +7,9 @@ scene = s.Scene()
 with scene:
     duck = s.Object.from_obj(str(Path("../meshes/cpsduck/cpsduck.obj").resolve()), object_id=1)
     cam = s.Camera("Camera")
+    s.Object.add_material(duck)
+
+    
 
 
 # export Scene as .blend file, so we can open it in Blender and check results
@@ -14,6 +17,6 @@ scene.export_blend(str(Path("scene.blend").resolve()))
 
 for i in range(10):
     s.random.randomize_rotation(duck)
-    s.random.randomize_in_camera_frustum(duck, cam, (1.0, 2.0), (0.9, 0.9))
-    
-    scene.render()
+    s.random.randomize_in_camera_frustum(duck, cam, (1.0, 3.0), (0.9, 0.9))
+    # Render the scene
+    scene.render(f"render/render_{i}.png")  # Save the rendered image to the specified file path
