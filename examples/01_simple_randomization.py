e7f317ba179686b3cb99fb4ad0ee924ae79522c9
sharanyakomma <96442414+sharanyakomma@users.noreply.github.com>
2023-06-19 17:18:32 +0530

    Update 01_simple_randomization.py

--- examples/01_simple_randomization.py
+++ examples/01_simple_randomization.py
@@ -3,30 +3,40 @@ from pathlib import Path
 import logging
 import numpy as np
 from tqdm import tqdm, trange
-
 logging.basicConfig(level=logging.INFO)
 
 scene = sp.Scene()
 
 
 # use context manager to explicitly specify the blender scene, where the objects are created
+objects = ["cpsduck","chew_toy","stapler"]
+obj = []
 with scene:
-    duck = sp.Object.from_obj(
-        str(Path(__file__).parent / Path("meshes/cpsduck/cpsduck.obj").resolve()),
-        object_id=1,
-    )
+    j = 0
+    for i in objects:
+        new = sp.Object.from_obj(
+        str(Path(__file__).parent / Path("meshes/"+i+"/"+i+".obj").resolve()),
+        object_id=j,
+        )
+        obj.append(new)
+        new.set_metallic_value(1.0)
+        new.set_roughness_value(0.4)
+        j = j+1
     cam = sp.Camera("Camera")
     light = sp.Light("Light", type="POINT", energy=100.0)
+    cam.add_background()
+
+
 
 
 
 light.set_location((1.0, 1.0, -0.2))
-duck.set_metallic_value(1.0)
-duck.set_roughness_value(0.4)
+
 
 for i in trange(10):
-    sp.random.randomize_rotation(duck)
-    sp.random.randomize_in_camera_frustum(duck, cam, (0.3, 1.0), (0.9, 0.9))
+    for j in obj:
+        sp.random.randomize_rotation(j)
+        sp.random.randomize_in_camera_frustum(j, cam, (0.3, 1.0), (0.9, 0.9))
     light.set_energy(np.random.uniform(10, 200))
     # Render the scene
 
