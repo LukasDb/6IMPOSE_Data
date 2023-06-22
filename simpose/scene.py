035dd8d0c3bf1107cd6c65a302e21b19b5024eaf
LukasDb <lukas.dirnberger@tum.de>
2023-06-22 14:06:01 +0200

    fixed depth

--- simpose/scene.py
+++ simpose/scene.py
@@ -31,19 +31,19 @@ class Scene:
         bpy.context.scene.render.use_persistent_data = True
         self.resolution = np.array([640, 480])
         bpy.context.scene.view_layers[0].cycles.use_denoising = True
-        self.Tree = None
+
         self.rgbnode = None
         self.depthnode = None
         self.segmentNode = None
         self._setup_compositor()
 
     def render(self, i):
-        self.segmentNode.file_slots[0].path = f"segment_{i}_"
-        self.rgbnode.file_slots[0].path = f"rgb_{i}_"
-        self.depthnode.file_slots[0].path = f"depth_{i}_"
-        bpy.context.scene.render.filepath = f"render/rgb_images/render_{i}.png"
+        bpy.context.scene.frame_set(i) # this sets the suffix for file names
+        self.segmentNode.file_slots[0].path = f"segment_"
+        self.rgbnode.file_slots[0].path = f"rgb_"
+        self.depthnode.file_slots[0].path = f"depth_"
         with redirect_stdout():
-            bpy.ops.render.render(write_still=True)
+            bpy.ops.render.render(write_still=False)
 
     def set_background(self, filepath):
         # get composition node_tree
@@ -67,14 +67,11 @@ class Scene:
     def _setup_compositor(self):
         bpy.context.scene.use_nodes = True
         bpy.context.scene.render.film_transparent = True
-        tree = bpy.context.scene.node_tree
-        self.Tree = tree
         bpy.context.view_layer.use_pass_z = True
-        
-
         bpy.context.view_layer.use_pass_object_index = True
-
         bpy.context.view_layer.use_pass_combined = True
+
+        tree = bpy.context.scene.node_tree
 
         # create a alpha node to overlay the rendered image over the background image
         alpha_over = tree.nodes.new("CompositorNodeAlphaOver")
@@ -83,36 +80,46 @@ class Scene:
         # create a transform node to scale the background image to fit the render resolution
         self.bg_transform = transform = tree.nodes.new("CompositorNodeTransform")
         transform.filter_type = "BILINEAR"
+
+        # invert alpha
+        invert_alpha = tree.nodes.new("CompositorNodeMath")
+        invert_alpha.operation = "SUBTRACT"
+        invert_alpha.inputs[0].default_value = 1.0
         
-    
-        # link the nodes
+        # bg_node -> transform
         tree.links.new(bg_image_node.outputs[0], transform.inputs[0])
-        tree.links.new(transform.outputs[0], alpha_over.inputs[1])
-        tree.links.new(tree.nodes["Render Layers"].outputs[0], alpha_over.inputs[2])
-        tree.links.new(alpha_over.outputs[0], tree.nodes["Composite"].inputs[0])
-        tree.links.new(tree.nodes["Render Layers"].outputs[1], alpha_over.inputs[0])
+        # transform -> alpha over
+        tree.links.new(transform.outputs[0], alpha_over.inputs[2])
+        # rendered_rgb -> alpha over
+        tree.links.new(tree.nodes["Render Layers"].outputs[0], alpha_over.inputs[1])
+        # rendered_alpha -> invert alpha
+        tree.links.new(tree.nodes["Render Layers"].outputs[1], invert_alpha.inputs[1])
+        # invert alpha -> alpha_over
+        tree.links.new(invert_alpha.outputs[0], alpha_over.inputs[0])
+
+
 
         # RGB image output
         rgb_output_node = tree.nodes.new("CompositorNodeOutputFile")
         rgb_output_node.base_path = "render/rgb/"
         rgb_output_node.format.file_format = "PNG"
         self.rgbnode = rgb_output_node
-        tree.links.new(self.Tree.nodes["Render Layers"].outputs["Image"], rgb_output_node.inputs["Image"])
+        tree.links.new(alpha_over.outputs[0], rgb_output_node.inputs["Image"])
         
         # Depth image output
         depth_output_node = tree.nodes.new("CompositorNodeOutputFile")
-        depth_output_node.base_path = "render/depth_images/"
-        depth_output_node.format.file_format = "PNG"
+        depth_output_node.base_path = "render/depth/"
+        depth_output_node.format.file_format = "OPEN_EXR"
         self.depthnode = depth_output_node
-        tree.links.new(self.Tree.nodes["Render Layers"].outputs["Depth"], depth_output_node.inputs["Image"])
+        tree.links.new(tree.nodes["Render Layers"].outputs["Depth"], depth_output_node.inputs["Image"])
         
         #id_mask_node = tree.nodes.new('CompositorNodeIDMask')
         segmentation_output_node = tree.nodes.new('CompositorNodeOutputFile')
-        segmentation_output_node.base_path = "render/segmentation_images/"
+        segmentation_output_node.base_path = "render/mask/"
         segmentation_output_node.format.file_format = "OPEN_EXR"
         self.segmentNode = segmentation_output_node
         
-        tree.links.new(self.Tree.nodes["Render Layers"].outputs['IndexOB'], segmentation_output_node.inputs['Image'])
+        tree.links.new(tree.nodes["Render Layers"].outputs['IndexOB'], segmentation_output_node.inputs['Image'])
         #tree.links.new(id_mask_node.outputs['IndexMA'], segmentation_output_node.inputs['Image'])
 
        
