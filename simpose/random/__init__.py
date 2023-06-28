f74229e58f59b452972b9a4e916ffd9c2b49cd9d
sharanyakomma <96442414+sharanyakomma@users.noreply.github.com>
2023-06-28 16:46:17 +0530

    Delete simpose/random directory

--- simpose/random/__init__.py
+++
@@ -1,4 +0,0 @@
-from .rand_rotation import randomize_rotation
-from .rand_in_camera_frustum import randomize_in_camera_frustum
-
-__all__ = ["randomize_rotation", "randomize_in_camera_frustum"]
--- simpose/random/rand_rotation.py
+++
@@ -1,6 +0,0 @@
-from scipy.spatial.transform import Rotation as R
-from ..placeable import Placeable
-
-
-def randomize_rotation(subject: Placeable):
-    subject.set_rotation(R.random())
