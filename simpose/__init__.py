4ef337cdb87c2cc2e2ee596abe769a6a7df7d25e
sharanyakomma <96442414+sharanyakomma@users.noreply.github.com>
2023-06-28 16:47:05 +0530

    Added rand file

--- simpose/__init__.py
+++ simpose/__init__.py
@@ -2,6 +2,6 @@ from simpose.scene import Scene
 from simpose.object import Object
 from simpose.camera import Camera
 from simpose.light import Light
-import simpose.random as random
+from simpose.rand import Random
 
-__all__ = ["Scene", "Object", "Camera", "random", "Light"]
+__all__ = ["Scene", "Object", "Camera", "Random", "Light"]
