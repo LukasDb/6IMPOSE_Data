58245da580ed1dabe60d69b1e227e35adc1615aa
sharanyakomma <96442414+sharanyakomma@users.noreply.github.com>
2023-06-28 16:51:57 +0530

    Used Random class

--- examples/01_simple_randomization.py
+++ examples/01_simple_randomization.py
@@ -20,24 +20,17 @@ with scene:
         new.set_metallic_value(1.0)
         new.set_roughness_value(0.5)
     cam = sp.Camera("Camera")
-    light = sp.Light("Light", type="POINT", energy=100.0)
-    light2 = sp.Light("Light2", type="POINT", energy=100.0)
-
-    bg = "//" + str(np.random.choice(list(Path("backgrounds").glob("*.jpg"))))
+    random = sp.Random
+    bg = random.random_background()
     scene.set_background(bg)
     
-
-light.set_location((1.0, 1.0, -0.2))
-light2.set_location((-1.0, -1.0, -0.2))
-
-
 for i in trange(10):
-    bg = "//" + str(np.random.choice(list(Path("backgrounds").glob("*.jpg"))))
+    bg = random.random_background()
     scene.set_background(bg)
     for j in obj:
-        sp.random.randomize_rotation(j)
-        sp.random.randomize_in_camera_frustum(j, cam, (0.3, 1.0), (0.9, 0.9))
-    light.set_energy(np.random.uniform(10, 200))
+        random.randomize_rotation(j)
+        random.randomize_in_camera_frustum(j, cam, (0.3, 1.0), (0.9, 0.9))
+    random.randomize_lighting((3,6),cam,(1.0,3.0),(40,100))
     # Render the scene
     scene.generate_data("render/gt/",obj,cam,i)  # Save the rendered image to the specified file path
 print(cam.get_calibration_matrix_K_from_blender())
